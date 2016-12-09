b1279b6f5822c930baad16b408081a9d03e5ee82
HJ08003 <hubertj@princeton.edu>
2016-12-09 09:43:19 -0500

    Updated the R code for the documentation
     To match what's already in the race.pred.Rd

--- R/race.pred.R
+++ R/race.pred.R
@@ -3,47 +3,54 @@
 #' \code{race.pred} makes probabilistic estimates of individual-level race/ethnicity.
 #'
 #' This function implements the Bayesian race prediction methods outlined in 
-#' Imai and Khanna (2015). The function produces probabilistics estimates of 
+#' Imai and Khanna (2015). The function produces probabilistic estimates of 
 #' individual-level race/ethnicity, based on surname, geolocation, and party.
-#' @param voters An object of class \code{data.frame}. Must contain field(s) 
-#'  named \code{\var{county}}, \code{\var{tract}}, and/or \code{\var{block}} 
-#'  specifying geolocation. These should be character variables that match up with 
-#'  U.S. Census categories. County should be three characters (e.g., "031" not "31"), 
-#'  tract should be six characters, and block should be four characters.
+#' @param voters An object of class \code{data.frame}. Must contain a row for each individual being 
+#' predicted, as well as a field named \code{\var{surname}} containing each individual's surname.
+#' If using geolocation in predictions, \code{\var{voters}} must contain a field named \code{\var{state}}, 
+#' which contains the two-character abbreviation for each individual's state of residence (e.g., "nj" for New Jersey). 
+#' If using geolocation, \code{\var{voters}} must also contain at least one of the following fields: 
+#' \code{\var{county}}, \code{\var{tract}}, and/or \code{\var{block}}. 
+#' These fields should contain character strings matching U.S. Census categories. 
+#' County is three characters (e.g., "031" not "31"), tract is six characters, and block is four characters. 
+#' See below for other optional fields.
 #' @param races A character vector specifying which racial groups to generate 
 #'  predicted probabilities for. Can include any subset of the default vector, 
 #'  which is \code{c("white", "black", "latino", "asian", "other")}.
 #' @param name.clean A \code{TRUE}/\code{FALSE} object. If \code{TRUE}, function will call
 #'  \code{name.clean} to merge in data from U.S. Census 2000 Surname List 
-#'  and Spanish Surname List. If \code{FALSE}, voters object must contain fields 
+#'  and Spanish Surname List. If \code{FALSE}, voters object must contain additional fields 
 #'  specifying Pr(Race | Surname), named as follows: 
 #'  \code{\var{p_whi}} for Whites, \code{\var{p_bla}} for Blacks, 
 #'  \code{\var{p_his}} for Hispanics/Latinos, \code{\var{p_asi}} for Asians, 
 #'  and/or \code{\var{p_oth}} for Other. Default is \code{TRUE}.
 #' @param census An optional character vector specifying what level of 
-#'  geography to use to merge in U.S. Census 2010 data. Currently only 
-#'  \code{county}, \code{tract}, and \code{"block"} are supported. 
-#'  If \code{tract}, \code{"tract"}, or \code{"block"} is specified, 
-#'  function will call \code{census.helper.api} to merge in Census data. 
-#'  If left unspecified, \code{voters} must contain fields 
-#'  specifying Pr(Geolocation | Race), including any of the following: 
-#'  \code{\var{r_whi}}, \code{\var{r_bla}}, \code{\var{r_his}}, 
-#'  \code{\var{r_asi}}, and/or \code{\var{r_oth}}.
+#' geography to use to merge in U.S. Census 2010 data. Can be one of 
+#' \code{"county"}, \code{"tract"}, or \code{"block"}. 
+#' Function calls \code{census.helper.api} to merge in Census data at specified level. 
+#' If left unspecified, \code{voters} must contain additional fields 
+#' specifying Pr(Geolocation | Race), including any of the following: 
+#' \code{\var{r_whi}}, \code{\var{r_bla}}, \code{\var{r_his}}, 
+#' \code{\var{r_asi}}, and/or \code{\var{r_oth}}. 
 #' @param census.key A character object specifying user's Census API 
-#'  key. Must be specified if \code{census} is specified, because the 
+#'  key. Required if \code{census} is specified, because 
 #'  \code{census.helper} function requires a Census API key to operate.
 #' @param demo An optional \code{TRUE}/\code{FALSE} object specifying whether to 
-#'  condition race predictions on individual age and sex. 
-#'  If \code{TRUE}, \code{voters} should include numerical variables 
-#'  \code{\var{age}} and \code{\var{sex}}, where \code{\var{sex}} coded as 0 for 
-#'  males and 1 for females. Default is \code{FALSE}. 
-#'  May only be set to \code{TRUE} when \code{census} is specified.
-#' @param party An optional character object specifying party registration field. 
-#'  Party registration should be coded as 1 for Democrat, 2 for Republican, and 
-#'  0 for Other.
-#' @param census.data A census data object, s list indexed by state names, which contains census data on demo, county, tract and block.
+#' condition race predictions on individual age and sex (in addition to geolocation). 
+#' Default is \code{FALSE}. 
+#' May only be set to \code{TRUE} if \code{census} option is specified. 
+#' If \code{TRUE}, \code{voters} should include numerical variables 
+#' \code{\var{age}} and \code{\var{sex}}, where \code{\var{sex}} coded as 0 for 
+#' males and 1 for females.
+#' @param party An optional character object specifying party registration field in \code{\var{voters}}, 
+#' e.g., \code{\var{party} = "PartyReg"}. If specified, race/ethnicity predictions will be conditioned 
+#' on individual's party registration (in addition to geolocation). 
+#' Whatever the name of the party registration field in \code{\var{voters}}, 
+#' it should be coded as 1 for Democrat, 2 for Republican, and 0 for Other.
+#' @param census.data A census data object, a list indexed by state names, 
+#' which contains census data on demo, county, tract and block.
 #' @param surname.only A \code{TRUE}/\code{FALSE} object. If \code{TRUE}, race predictions will 
-#'  only use surname data and calculate Pr(Race | Surnname). Default is \code{FALSE}
+#'  only use surname data and calculate Pr(Race | Surnname). Default is \code{FALSE}.
 #' @return Output will be an object of class \code{data.frame}. It will 
 #'  consist of the original user-input data with additional columns that 
 #'  contain predicted probabilities for each race in \code{races}.
