756363578ac491c5bbe953ce29cb8ba807f8f068
HJ08003 <hubertj@princeton.edu>
2017-08-21 19:19:20 -0400

    test_that() and expect_that() added.

--- tests/testthat/test-census-key.R
+++ tests/testthat/test-census-key.R
@@ -1,12 +1,22 @@
-# Set random seed
-set.seed(12345)
-
-data(voters)
+context("tests predictions using the census key")
+rm(list=ls())
 
 # Need a valid census key
 k <- NULL
 
-if (!is.null(k)) {
-  predict_race(voter.file = voters, census.geo = "tract", census.key = k, party = "PID")
-}
+test_that("tests predictions using the census key", {
+  # Set random seed
+  set.seed(12345)
+
+  data(voters)
+
+  if (!is.null(k)) {
+    x = predict_race(voter.file = voters, census.geo = "tract", census.key = k, party = "PID")
+    expect_that(dim(x), is_equivalent_to(c(10,18)))
+    expect_that(sum(is.na(x)), is_equivalent_to(0))
+    expect_that(sum(x$surname == "Johnson"), is_equivalent_to(1))
+    expect_that(round(x[x$surname == "Khanna", "pred.whi"], 4), is_equivalent_to(0.0819))
+    expect_that(round(x[x$surname == "Morse", "pred.his"], 4), is_equivalent_to(0.0034))
+  }
+})
 
--- tests/testthat/test-census-object.R
+++ tests/testthat/test-census-object.R
@@ -1,27 +1,47 @@
-# Set random seed
-set.seed(12345)
-
-data(voters)
+context("tests predictions using the census object")
+rm(list=ls())
 
 # Need a valid census key
 k <- NULL
 
-if (!is.null(k)) {
-  voters.dc.nj <- voters[c(-3, -7), ]  # remove two NY cases from dataset
-  census.dc.nj <- get_census_data(key = k, state = c("DC", "NJ"), census.geo = "tract", age = TRUE, sex = FALSE)  # create Census data object covering DC and NJ 
-  predict_race(voter.file = voters.dc.nj, census.geo = "tract", census.data = census.dc.nj, age = TRUE, sex = FALSE, party = "PID")
+test_that("tests predictions using the census object", {
+  # Set random seed
+  set.seed(12345)
 
-  # Or build the census object by parts
-  censusObj2  <- list()
-  county.dc <- census_geo_api(key = k, state = "DC", geo = "county", age = TRUE, sex = FALSE)
-  tract.dc <- census_geo_api(key = k, state = "DC", geo = "tract", age = TRUE, sex = FALSE)
-  censusObj2[["DC"]] <- list(state = "DC", county = county.dc, tract = tract.dc, age = TRUE, sex = FALSE)
-  tract.nj <- census_geo_api(key = k, state = "NJ", geo = "tract", age = TRUE, sex = FALSE)
-  county.nj <- census_geo_api(key = k, state = "NJ", geo = "county", age = TRUE, sex = FALSE)
-  censusObj2[["NJ"]] <- list(state = "NJ", county = county.nj, tract = tract.nj, age = TRUE, sex = FALSE)
+  data(voters)
 
-  predict_race(voter.file = voters.dc.nj, census.geo = "county", census.data = censusObj2, age = TRUE, sex = FALSE)  # Pr(Race | Surname, County)
-  predict_race(voter.file = voters.dc.nj, census.geo = "tract", census.data = censusObj2, age = TRUE, sex = FALSE)  # Pr(Race | Surname, Tract)
-  predict_race(voter.file = voters.dc.nj, census.geo = "county", census.data = censusObj2, party = "PID", age = TRUE, sex = FALSE)  # Pr(Race | Surname, County, Party)
-  predict_race(voter.file = voters.dc.nj, census.geo = "tract", census.data = censusObj2, party = "PID", age = TRUE, sex = FALSE)  # Pr(Race | Surname, Tract, Party)
-}
+  if (!is.null(k)) {
+    voters.dc.nj <- voters[c(-3, -7), ]  # remove two NY cases from dataset
+    census.dc.nj <- get_census_data(key = k, state = c("DC", "NJ"), census.geo = "tract", age = TRUE, sex = FALSE)  # create Census data object covering DC and NJ 
+    x = predict_race(voter.file = voters.dc.nj, census.geo = "tract", census.data = census.dc.nj, age = TRUE, sex = FALSE, party = "PID")
+    expect_that(dim(x), is_equivalent_to(c(8,18)))
+    expect_that(sum(is.na(x)), is_equivalent_to(0))
+    expect_that(sum(x$surname == "Johnson"), is_equivalent_to(0))
+    expect_that(round(x[x$surname == "Khanna", "pred.whi"], 4), is_equivalent_to(0.0644))
+    expect_that(round(x[x$surname == "Morse", "pred.his"], 4), is_equivalent_to(0.0042))
+    
+    # Or build the census object by parts
+    censusObj2  <- list()
+    county.dc <- census_geo_api(key = k, state = "DC", geo = "county", age = TRUE, sex = FALSE)
+    tract.dc <- census_geo_api(key = k, state = "DC", geo = "tract", age = TRUE, sex = FALSE)
+    censusObj2[["DC"]] <- list(state = "DC", county = county.dc, tract = tract.dc, age = TRUE, sex = FALSE)
+    tract.nj <- census_geo_api(key = k, state = "NJ", geo = "tract", age = TRUE, sex = FALSE)
+    county.nj <- census_geo_api(key = k, state = "NJ", geo = "county", age = TRUE, sex = FALSE)
+    censusObj2[["NJ"]] <- list(state = "NJ", county = county.nj, tract = tract.nj, age = TRUE, sex = FALSE)
+
+    x = predict_race(voter.file = voters.dc.nj, census.geo = "county", census.data = censusObj2, age = TRUE, sex = FALSE)  # Pr(Race | Surname, County)
+    expect_that(dim(x), is_equivalent_to(c(8,18)))
+    expect_that(sum(is.na(x)), is_equivalent_to(0))
+    expect_that(sum(x$surname == "Johnson"), is_equivalent_to(0))
+    expect_that(round(x[x$surname == "Khanna", "pred.whi"], 4), is_equivalent_to(0.0441))
+    expect_that(round(x[x$surname == "Morse", "pred.his"], 4), is_equivalent_to(0.0163))
+    
+    x = predict_race(voter.file = voters.dc.nj, census.geo = "tract", census.data = censusObj2, party = "PID", age = TRUE, sex = FALSE)  # Pr(Race | Surname, Tract, Party)
+    expect_that(dim(x), is_equivalent_to(c(8,18)))
+    expect_that(sum(is.na(x)), is_equivalent_to(0))
+    expect_that(sum(x$surname == "Johnson"), is_equivalent_to(0))
+    expect_that(round(x[x$surname == "Khanna", "pred.whi"], 4), is_equivalent_to(0.0644))
+    expect_that(round(x[x$surname == "Morse", "pred.his"], 4), is_equivalent_to(0.0042))
+  }
+})
+
