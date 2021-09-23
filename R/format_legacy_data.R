65b86356c1a30fbf417e48bdcd647af7c8fa213a
etrrosenman <etrrosenman@gmail.com>
2021-09-22 17:17:35 -0700

    latest

--- R/format_legacy_data.R
+++ R/format_legacy_data.R
@@ -8,18 +8,23 @@
 #' https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/. 
 #' This function returns data structured analogously to data from the Census API, which is not yet 
 #' available for the 2020 Census as of September 2021. 
-#' 
 #'
 #' @param legacyFilePath A character vector giving the location of a legacy census data folder,
 #' sourced from https://www2.census.gov/programs-surveys/decennial/2020/data/01-Redistricting_File--PL_94-171/. 
 #' These file names should end in ".pl". 
-#' @param outFile A character vector determining whether the formatted RData object should be saved. The
+#' @param outFile Optional character vector determining whether the formatted RData object should be saved. The
 #' filepath should end in ".RData". 
 #'
 #' @import PL94171
+#' 
+#' @examples
+#' gaCensusData <- format_legacy_data('~/Desktop/ga2020.pl')
+#' predict_race_new(ga.voter.file, namesToUse = 'last, first, mid', census.geo = 'block',
+#'      census.data = gaCensusData)
+
 #'
 #' @export
-formatData <- function(legacyFilePath, outFile) {
+format_legacy_data <- function(legacyFilePath, outFile = NULL) {
   
   # aggregation levels to convert (county, tract, block group, and block)
   summaryLevels <- c('050', '140', '150', '750')
@@ -57,8 +62,11 @@ formatData <- function(legacyFilePath, outFile) {
     df
   })
   
-  # format and save the file 
+  # format and optionally save the file 
   names(censusData.2020) <- c('county', 'tract', 'blockGroup', 'block')
-  save(censusData.2020, file = outFile)
+  if(!is.null(outFile))
+    save(censusData.2020, file = outFile)
   
+  # return the object
+  return(censusData.2020)
 }
