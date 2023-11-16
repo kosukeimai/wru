60c1cd26e4e39953bf20de3ff20054cb256dc735
Alexander Rossell Hayes <alexander@rossellhayes.com>
2023-11-15 16:09:09 -0800

    feat: add helper function `validate_key()`

---
+++ R/validate_key.R
@@ -0,0 +1,45 @@
+validate_key <- function(key, argument_name = NULL, call = rlang::caller_call()) {
+  if (is.null(argument_name)) {
+    argument_name <- rlang::expr_text(rlang::enexpr(key))
+  }
+  
+  if (length(key) != 1) {
+    cli::cli_abort(
+      c(
+        "{.arg {argument_name}} must be a {.cls character} string of length {.val {1}}.",
+        x = "{.arg {argument_name}} has a length of {.val {length(key)}}."
+      ),
+      call = call
+    )
+  }
+  
+  if (!inherits(key, "character")) {
+    cli::cli_abort(
+      c(
+        "{.arg {argument_name}} must be a {.cls character} string of length {.val {1}}.",
+        x = "{.arg {argument_name}} is an object of class {.cls {class(key)}}."
+      ),
+      call = call
+    )
+  }
+  
+  if (!nzchar(key)) {
+    cli::cli_abort(
+      c(
+        "{.arg {argument_name}} must not be an empty string.",
+        "*" = "Have you set the {.envvar CENSUS_API_KEY} environment variable?
+        See {.help wru::get_census_data} for more information."
+      ),
+      call = call
+    )
+  }
+  
+  if (is.na(key)) {
+    cli::cli_abort(
+      "{.arg {argument_name}} must not be {.val {NA_character_}}.",
+      call = call
+    )
+  }
+  
+  key
+}
