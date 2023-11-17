926c2c1f20d63e360137b010bc72f8f570fdf183
Alexander Rossell Hayes <alexander@rossellhayes.com>
2023-11-17 12:24:10 -0800

    feat: add CITATION file and use it to generate package startup message

--- R/wru-internal.R
+++ R/wru-internal.R
@@ -1,7 +1,7 @@
-.onAttach <- 
-function(libname, pkgname) {
-  packageStartupMessage("\nPlease cite as: \n")
-  packageStartupMessage("Khanna K, Bertelsen B, Olivella S, Rosenman E, Imai K (2022). wru: Who are You?")
-  packageStartupMessage("Bayesian Prediction of Racial Category Using Surname, First Name, Middle Name, and Geolocation.")
-  packageStartupMessage("URL: https://CRAN.R-project.org/package=wru \n")
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "\nPlease cite as:\n\n",
+    format(citation("wru"), style = "text"),
+    "\n"
+  )
 }
