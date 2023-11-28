45618545a234d00862bfb3999aab33ef0ff34dbf
Alexander Rossell Hayes <alexander@rossellhayes.com>
2023-11-28 13:29:40 -0800

    feat(get_census_data): add support for `census.geo = "zcta"`

--- R/get_census_data.R
+++ R/get_census_data.R
@@ -36,20 +36,23 @@
 #' 
 #' @export
 #'
-#' @examples 
+#' @examples
 #' \dontrun{get_census_data(states = c("NJ", "NY"), age = TRUE, sex = FALSE)}
 #' \dontrun{get_census_data(states = "MN", age = FALSE, sex = FALSE, year = "2020")}
 get_census_data <- function(
-  key = Sys.getenv("CENSUS_API_KEY"),
-  states,
-  age = FALSE,
-  sex = FALSE,
-  year = "2020",
-  census.geo = "block",
-  retry = 3,
-  county.list = NULL
+    key = Sys.getenv("CENSUS_API_KEY"),
+    states,
+    age = FALSE,
+    sex = FALSE,
+    year = "2020",
+    census.geo = c("tract", "block", "block_group", "county", "place", "zcta"),
+    retry = 3,
+    county.list = NULL
 ) {
   validate_key(key)
+  
+  census.geo <- tolower(census.geo)
+  census.geo <- rlang::arg_match(census.geo)
   
   states <- toupper(states)
   
@@ -80,6 +83,18 @@ get_census_data <- function(
     if ((census.geo == "block") || (census.geo == "tract") || (census.geo == "county") || (census.geo == "block_group")) {
       county <- census_geo_api(key, s, geo = "county", age, sex, year, retry)
       CensusObj[[s]]$county <- county
+    }
+    
+    if (census.geo == "zcta") {
+      if (!is.null(county.list)) {
+        cli::cli_abort(c(
+          "The {.arg county.list} argument must be set to {.code NULL}
+          when {.arg census_geo} is {.val zcta},
+          because the Census Bureau does release data that divides ZCTAs by county."
+        ))
+      }
+      
+      CensusObj[[s]]$zcta <- census_geo_api(key, s, geo = "zcta", age, sex, year, retry)
     }
   }
   return(CensusObj)
