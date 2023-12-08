4ff2f0f42e1748c52b74ac45d9407c997600ae0b
Alexander Rossell Hayes <alexander@rossellhayes.com>
2023-12-08 11:12:57 -0800

    test(census_geo_api): add comment explaining snapshots

--- tests/testthat/test-census_geo_api.R
+++ tests/testthat/test-census_geo_api.R
@@ -1,6 +1,8 @@
 skip_if_not(nzchar(Sys.getenv("CENSUS_API_KEY")))
 
 test_that("snapshot", {
+  # These snapshots were generated using the calculations in v2.0.0
+  # and verified that the calculations resulted in the same numbers for PR #120.
   expect_snapshot_value(
     census_geo_api(state = "DE", geo = "county", year = "2020"),
     style = "deparse"
